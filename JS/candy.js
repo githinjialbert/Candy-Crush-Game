9eafd6a644957fc891e499d39091bfb76331da55
githinjialbert <githinjialbert001@gmail.com>
2024-08-31 17:52:48 +0300

    Adding functionality for the tiles and the way they move

---
+++ JS/candy.js
@@ -0,0 +1,43 @@
+const candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
+const board = [];
+const rows = 9;
+const columns = 9;
+let currentTile;
+let otherTile;
+const myBoard = document.getElementById("board");
+
+window.onload(() => {
+    startGame();
+
+    window.setInterval(() => {
+        crushCandy();
+        slideCandy();
+        generateCandy();
+    }, 150);
+});
+
+const randomCandy = () => {
+    return candies[Math.floor(Math.random() * candies.length)];
+}
+
+const startGame = () => {
+    for (let r = 0; r < rows; r++) {
+
+        let rows = [];
+
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("img");
+
+            tile.id  = r.toString() + "-" + c.toString();
+            tile.src = "./images/" + randomCandy() + ".png";
+
+            tile.addEventListener("dragstart", dragStart); // click and initialize the drag process
+            tile.addEventListener("dragover", dragOver); // click on candy, moving mouse to drag the candy
+            tile.addEventListener("dragenter", dragEnter); // dragging candy onto another candy
+            tile.addEventListener("dragleave", dragLeave); // leave candy over the other candy
+            tile.addEventListener("drop", dragDrop); // drop candy over other candy
+            tile.addEventListener("dragend", dragEnd); // after drag process is completed, we swap candies
+
+        }
+    }
+}
